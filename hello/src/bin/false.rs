7ca30743015a5de630404746a5913ad91fd046cb
Drew Conner <drew@blinkingboxes.net>
2023-11-21 07:43:51 -0600

    added true, false, and tests for each

---
+++ hello/src/bin/false.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // std::process::exit(1);
+    // abort below got a core dumped message and
+    // exit code was 134
+    std::process::abort();
+    // both of the above pass the test in cli.rs
+}
--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -2,6 +2,18 @@ use assert_cmd::Command;
 
 #[test]
 fn runs() {
-    let mut cmd = Command::cargo_bin('hello').unwrap();
+    let mut cmd = Command::cargo_bin("hello").unwrap();
     cmd.assert().success();
+}
+
+#[test]
+fn true_ok() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
+    cmd.assert().success();
+}
+
+#[test]
+fn false_not_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
 }
